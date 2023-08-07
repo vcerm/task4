2c0a5eb7592e07379709b3acad3c35e669ff9a7b
main <malekseev8@mail.ru>
2023-08-07 14:39:47 +0300

    First commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,12 @@
 package app.model;
 
-public class Timer {
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer{
 
     private Long nanoTime = System.nanoTime();
-
     public Long getTime() {
         return nanoTime;
     }
